e0e52dad7132537ad7f9221e280a83c0c6bab25c
DX4GREY <dxablack@gmail.com>
2025-07-02 06:52:44 +0000

    Replace 4-space to tab for all
    Signed-off-by: DX4GREY <dxablack@gmail.com>

--- hackbyte/dummyproc.py
+++ hackbyte/dummyproc.py
@@ -1,4 +1,4 @@
 class DummyProcess:
-    def __init__(self):
-        self.pid = 9999
-        print("[!] This is dummy mode. No real processes are used.")
+	def __init__(self):
+		self.pid = 9999
+		print("[!] This is dummy mode. No real processes are used.")
